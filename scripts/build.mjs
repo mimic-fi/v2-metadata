9caf31db36e25a4b437e41e4bf0fc887fa551b95
brunitob <brunitob@gmail.com>
2022-11-02 10:34:09 -0300

    update script and json structure

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,27 +8,34 @@ for (const directory of fs.readdirSync(baseLocation)) {
   const __dirname = `${baseLocation}/${directory}`;
 
   for (const name of fs.readdirSync(__dirname)) {
-    console.log(`New ${directory}: `, name);
     const file = path.join(__dirname, name);
     const stat = fs.lstatSync(file);
+
     if (stat.isFile()) {
       const parse = path.parse(file);
       if (parse.ext === ".json") {
         const source = JSON.parse(fs.readFileSync(file));
         const networks = source.networks;
+        const addresses = source.addresses;
+
         for (const network of networks) {
-          const __outputDirname = `${outputLocation}/${directory}/${network}`;
-          const copy = { ...source };
-          delete copy.networks;
+          for (const address of addresses) {
+            console.log(`New ${directory}: `, name, network, address);
 
-          if (!fs.existsSync(__outputDirname)) {
-            fs.mkdirSync(__outputDirname, { recursive: true });
+            const __outputDirname = `${outputLocation}/${directory}/${network}`;
+            const copy = { ...source };
+            delete copy.networks;
+            delete copy.addresses;
+
+            if (!fs.existsSync(__outputDirname)) {
+              fs.mkdirSync(__outputDirname, { recursive: true });
+            }
+
+            fs.writeFileSync(
+              path.join(__outputDirname, address),
+              JSON.stringify(copy)
+            );
           }
-
-          fs.writeFileSync(
-            path.join(__outputDirname, copy.address),
-            JSON.stringify(copy)
-          );
         }
       }
     }
