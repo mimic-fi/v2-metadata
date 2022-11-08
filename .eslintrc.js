ef650d50f881abcb88a1d6ffc12da1712ca9d339
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-11-08 06:57:46 -0300

    chore: fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-    parser: 'babel-eslint',
-    extends: [
-      'react-app'
-    ],
-    rules: {
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'never'],
-      'max-len': ['error', { code: 120, ignoreStrings: true }],
-      'comma-spacing': ['error', { before: false, after: true }]
-    },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 8,
+    requireConfigFile: false,
+  },
+  rules: {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'max-len': ['error', { code: 120, ignoreStrings: true }],
+    'comma-spacing': ['error', { before: false, after: true }]
   }
+}
--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,40 +1,40 @@
-import fs from "fs-extra";
-import path from "path";
+import fs from 'fs-extra'
+import path from 'path'
 
-const baseLocation = "./data";
-const outputLocation = "./build";
+const baseLocation = './data'
+const outputLocation = './build'
 
 for (const directory of fs.readdirSync(baseLocation)) {
-  const __dirname = `${baseLocation}/${directory}`;
+  const __dirname = `${baseLocation}/${directory}`
 
   for (const name of fs.readdirSync(__dirname)) {
-    const file = path.join(__dirname, name);
-    const stat = fs.lstatSync(file);
+    const file = path.join(__dirname, name)
+    const stat = fs.lstatSync(file)
 
     if (stat.isFile()) {
-      const parse = path.parse(file);
-      if (parse.ext === ".json") {
-        const source = JSON.parse(fs.readFileSync(file));
-        const networks = source.networks;
-        const addresses = source.addresses;
+      const parse = path.parse(file)
+      if (parse.ext === '.json') {
+        const source = JSON.parse(fs.readFileSync(file))
+        const networks = source.networks
+        const addresses = source.addresses
 
         for (const network of networks) {
           for (const address of addresses) {
-            console.log(`New ${directory}: `, name, network, address);
+            console.log(`New ${directory}: `, name, network, address)
 
-            const __outputDirname = `${outputLocation}/${directory}/${network}`;
-            const copy = { ...source };
-            delete copy.networks;
-            delete copy.addresses;
+            const __outputDirname = `${outputLocation}/${directory}/${network}`
+            const copy = { ...source }
+            delete copy.networks
+            delete copy.addresses
 
             if (!fs.existsSync(__outputDirname)) {
-              fs.mkdirSync(__outputDirname, { recursive: true });
+              fs.mkdirSync(__outputDirname, { recursive: true })
             }
 
             fs.writeFileSync(
               path.join(__outputDirname, address),
               JSON.stringify(copy)
-            );
+            )
           }
         }
       }
